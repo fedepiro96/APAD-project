db58627f9654fd5dcec79d3f589f0b3ad0b0b988
bencinoemi <memi96.nb@gmail.com>
2020-06-18 13:48:22 +0200

    Add .gitignore

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 import json
 import networkx as nx
 
-with open('G:/Algoritmi e programmazione/Progetto/Dati/dpc-covid19-ita-province.json') as f:
-         d = json.load(f) 
-
+#with open('G:/Algoritmi e programmazione/Progetto/Dati/dpc-covid19-ita-province.json') as f:
+#         d = json.load(f)
+'''
 type(d)  #list
 type(d[1]) #dictionary
 today = d[len(d)-1].get("data")
@@ -21,5 +21,5 @@ for i in range(n):
     if copy[i].get("lat") == 0:
        newd.pop(i)
 
-len(newd)   #107, sembra corretto
+len(newd)   #107, sembra corretto'''
 
