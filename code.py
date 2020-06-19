3a0037dc5ddf84fcef4983b20a3523c6100bbe79
fedepiro96 <federico.pirona@stud.unifi.it>
2020-06-19 12:29:17 +0200

    avanti

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 import json
 import networkx as nx
+import numpy as np
 
-with open('G:/Algoritmi e programmazione/Progetto/Dati/dpc-covid19-ita-province.json') as f:
+with open('G:/Algoritmi e programmazione/Progetto/APAD-project/dpc-covid19-ita-province.json') as f:
          d = json.load(f) 
 
 type(d)  #list
@@ -15,11 +16,48 @@ while d[begin].get("data") == today:
     begin -= 1
 newd = d[begin:]
 
-n = len(newd)
-copy = newd[:]
-for i in range(n):
-    if copy[i].get("lat") == 0:
-       newd.pop(i)
 
-len(newd)   #107, sembra corretto
+G = nx.Graph()
+for i in range(len(newd)):
+    if newd[i].get("lat") != 0: 
+       G.add_node(newd[i]["sigla_provincia"], lat = newd[i]["lat"], 
+                  long = newd[i]["long"])
+
+G.nodes["FI"]
+G.nodes["FI"]["lat"]
+
+def nearlat(v, w, d = .8):
+    return abs(v["lat"] - w["lat"]) < d
+
+def nearlong(v, w, d = .8):
+    return abs(v["long"] - w["long"]) < d
+
+
+
+nearlat(G.nodes["FI"], G.nodes["AR"])
+dic = nx.get_node_attributes(G, "lat") #Ottengo dic con chiavi prov, values lat
+sortlat = sorted(dic, key= dic.get) #Province ordinate in base a latitudine
+
+
+
+def set_edges(G, ordered = sortlat):
+    n = len(ordered) -1
+    i, j = 0,1
+    while i < n:
+        if nearlat(G.nodes[ordered[i]], G.nodes[ordered[j]]):
+            if nearlong(G.nodes[ordered[i]], G.nodes[ordered[j]]):
+                G.add_edge(ordered[i], ordered[j])
+            if j < n:
+                j += 1
+            else:
+                i += 1
+        else:
+            i += 1
+            j = i+1
+    return
+    
+set_edges(G)    
+G.number_of_edges()
+
+    
 
