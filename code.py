69dfd19a8c7299855a985aae4e73cdbb5bb4e541
bencinoemi <memi96.nb@gmail.com>
2020-06-22 18:16:49 +0200

    remove venv stuff

--- code.py
+++ code.py
@@ -1,18 +1,25 @@
+# libraries
 import json
 import networkx as nx
-import numpy as np
+from random import random
+from random import randrange
+import matplotlib.pyplot as plt
 
+
+# open data
 with open('/home/noe/Università/in_corso/Algoritmi/APAD-project/dati/dati-json/dpc-covid19-ita-province.json') as f:
     d = json.load(f)
 
+# preparing data to build the graph
 lats = {}
 longs = {}
-for record in d:                                                # per ogni osservazione
-    if record['lat'] != 0 and record['long'] != 0:              # elimino i record sbagliati
-        if record['sigla_provincia'] not in lats.keys():        # memorizzo solo la provincia e la sua lat e long
-            lats[record['sigla_provincia']] = record['lat']     # in due dizionari lats e longs
+for record in d:                                                # for each observation
+    if record['lat'] != 0 and record['long'] != 0:              # drop the wrong observation (lat or long = 0)
+        if record['sigla_provincia'] not in lats.keys():        # store the name of the city and it latitude
+            lats[record['sigla_provincia']] = record['lat']     # and longitude in two dictionaries: lats e longs
             longs[record['sigla_provincia']] = record['long']
 
+# sort the list of cities by its latitude
 prov_sort = sorted(lats, key=lats.get)
 
 def graph_builder(node_list_sort, lat, long, dist_max, weight=False):
@@ -21,13 +28,14 @@ def graph_builder(node_list_sort, lat, long, dist_max, weight=False):
     node_list_sort: list of cities sorted by latitudes
     lat: dictionary where each node (key) is associated to its latitude
     long: dictionary where each node (key) is associated to its longitude
-    if weight=True the edges are weighted with the Euclidean distance between the cities"""
+    if weight=True the edges are weighted with the Euclidean distance between the cities
+    """
     G = nx.Graph()
     dist = {}
     i = 0
     j = 1
     while i <= (len(node_list_sort)-1):
-        G.add_node(node_list_sort[i])
+        G.add_node(node_list_sort[i], lat=lat[node_list_sort[i]], long=long[node_list_sort[i]])
         if j <= (len(node_list_sort)-1) and abs(lat[node_list_sort[i]] - lat[node_list_sort[j]]) < dist_max:
             if abs(long[node_list_sort[i]] - long[node_list_sort[j]]) < dist_max:
                 G.add_edge(node_list_sort[i], node_list_sort[j])
@@ -46,13 +54,9 @@ def graph_builder(node_list_sort, lat, long, dist_max, weight=False):
 
 
 P = graph_builder(prov_sort, lats, longs, dist_max=0.8)
-# print(P.nodes())
+# print(P.nodes.data())
 # print(P.edges())
 # print(len(P.edges()))
-
-from random import random
-from random import randrange
-print(randrange(30, 49))
 
 def generate_x():
     """Random generator for latitude"""
@@ -89,24 +93,29 @@ def random_graph(n):
 rand_nodes, rand_lat, rand_long = random_graph(2000)
 
 rand_graph1 = graph_builder(sorted(rand_lat, key=rand_lat.get), rand_lat, rand_long, dist_max=0.8)
-# print(rand_graph1.nodes())
-# print(rand_graph1.edges())
 # print(len(rand_graph1.edges()))
 
 R = graph_builder(prov_sort, lats, longs, dist_max=0.08)
-# print(R.nodes())
-# print(R.edges())
 # print(len(R.edges()))
 
 rand_graph2 = graph_builder(sorted(rand_lat, key=rand_lat.get), rand_lat, rand_long, dist_max=0.08)
-# print(rand_graph2.nodes())
-# print(rand_graph2.edges())
 # print(len(rand_graph2.edges()))
 
 P_weight = graph_builder(prov_sort, lats, longs, dist_max=0.8, weight=True)
-print(nx.get_edge_attributes(P_weight, 'distance'))
+# print(nx.get_edge_attributes(P_weight, 'distance'))
 R_weight = graph_builder(prov_sort, lats, longs, dist_max=0.08, weight=True)
-print(nx.get_edge_attributes(R_weight, 'distance'))
+# print(nx.get_edge_attributes(R_weight, 'distance'))
+
+# nx.draw_random(P, with_labels=True)
+# plt.savefig("/home/noe/Università/in_corso/Algoritmi/APAD-project/mygraph.png")
+
+# counting triangles
+nodes = P.nodes()
+numb_triangles = 0
+
+print(prov_sort)
+
+
 
 
 
